6515eee02a2eb35316ab1139db9af0379cc25d15
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-24 00:04:55 +0530

    feat: added counter implementation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Counter</Text>
+      <View>
+        <Text style={styles.result}>{count}</Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <View style={styles.btnStyle}>
+          <Button
+            title="+"
+            onPress={() => setCount(count + 1)}
+            color="lightgreen"
+          />
+        </View>
+        <View style={styles.btnStyle}>
+          <Button
+            title=" - "
+            onPress={() => count > 0 && setCount(count - 1)}
+            color="red"
+          />
+        </View>
+        <View style={styles.btnStyle}>
+          <Button title=" reset " onPress={() => setCount(0)} />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +38,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    // justifyContent: 'center',
   },
-});
+  heading: {
+    fontSize: 25,
+  },
+  btnContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  btnStyle: {
+    paddingTop: 20,
+    paddingRight: 10,
+  },
+  result: {
+    paddingTop: 10,
+    fontSize: 22,
+  },
+})
